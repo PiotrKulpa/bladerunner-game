c9a275eadcc450b0641d704cdf458aee4a6da3e7
piotrkulpa-collab <piotr.kulpa@leocode.com>
2026-04-15 19:46:04 +0200

    init repo

---
+++ src/game/main.ts
@@ -0,0 +1,30 @@
+import { Boot } from "./scenes/Boot";
+import { GameOver } from "./scenes/GameOver";
+import { Game as MainGame } from "./scenes/Game";
+import { MainMenu } from "./scenes/MainMenu";
+import { AUTO, Game, Scale } from "phaser";
+import { Preloader } from "./scenes/Preloader";
+
+//  Find out more information about the Game Config at:
+//  https://docs.phaser.io/api-documentation/typedef/types-core#gameconfig
+const config: Phaser.Types.Core.GameConfig = {
+  type: AUTO,
+  width: 1280,
+  height: 720,
+  parent: "game-container",
+  pixelArt: true,
+  antialias: false,
+  roundPixels: true,
+  scale: {
+    mode: Scale.FIT,
+    autoCenter: Scale.CENTER_BOTH,
+  },
+  backgroundColor: "#028af8",
+  scene: [Boot, Preloader, MainMenu, MainGame, GameOver],
+};
+
+const StartGame = (parent: string) => {
+  return new Game({ ...config, parent });
+};
+
+export default StartGame;
---
+++ src/game/scenes/Game.ts
@@ -0,0 +1,36 @@
+import { Scene } from 'phaser';
+
+export class Game extends Scene
+{
+    camera: Phaser.Cameras.Scene2D.Camera;
+    background: Phaser.GameObjects.Image;
+    msg_text : Phaser.GameObjects.Text;
+
+    constructor ()
+    {
+        super('Game');
+    }
+
+    create ()
+    {
+        this.camera = this.cameras.main;
+        this.camera.setBackgroundColor(0x00ff00);
+        const { centerX, centerY } = this.camera;
+
+        this.background = this.add.image(centerX, centerY, 'background');
+        this.background.setAlpha(0.5);
+
+        this.msg_text = this.add.text(centerX, centerY, 'Make something fun!\nand share it with us:\nsupport@phaser.io', {
+            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
+            stroke: '#000000', strokeThickness: 8,
+            align: 'center'
+        });
+        this.msg_text.setOrigin(0.5);
+
+        this.input.once('pointerdown', () => {
+
+            this.scene.start('GameOver');
+
+        });
+    }
+}
